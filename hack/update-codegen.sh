d376e692b4f1ab0e521ff05d9520517310f284c1
huanwei <huan@harmonycloud.cn>
2018-08-10 00:13:01 +0800

    hack
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -3,6 +3,6 @@
 cd $GOPATH/src/k8s.io/code-generator
 chmod +x generate-groups.sh
 bash generate-groups.sh "all" \
-github.com/oracle/mysql-operator/pkg/generated \
-github.com/oracle/mysql-operator/pkg/apis \
-"mysql:v1"
+github.com/huanwei/mysql-operator/pkg/generated \
+github.com/huanwei/mysql-operator/pkg/apis \
+"mysql:v1alpha1"
